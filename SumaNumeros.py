a6ebe9ff403b3603ffbffb991eb98efe2f51ad13
Neymarjr <neymarjr@gmail.com>
2025-03-10 23:42:47 +0100

    SumaNumeros

---
+++ SumaNumeros.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingresa un numero : "))
+numero2 = int(input("Ingresa otro numero : "))
+
+resultado = numero1 + numero2
+
+print(f"La suma de los dos numeros es : {resultado}")
+
